5b2cbcb928f537dbd42710c73e8cab7acd707c7c
butoCakil <bennysurahman@gmail.com>
2023-11-14 21:06:19 +0700

    fix - log error

--- app/mqtt/sub_mqtt.php
+++ app/mqtt/sub_mqtt.php
@@ -78,6 +78,44 @@ function procmsg($topic, $msg)
 
     if (!$response) {
         $text_log .= "Tidak ada Respon dari \"directagJSON\".\n\n";
+
+        echo $text_log;
+
+        // Mendapatkan tanggal hari ini
+        $tanggal = date("Y-m-d");
+
+        // Direktori penyimpanan file log (ganti dengan direktori yang diinginkan)
+        $lokasi_penyimpanan = "/opt/lampp/htdocs/app/mqtt/log/";
+
+        // Nama file log
+        $nama_file_log = $lokasi_penyimpanan . "tag_" . $tanggal . ".log";
+
+        // Mengecek apakah file sudah ada atau belum
+        if (!file_exists($nama_file_log)) {
+            // Jika file belum ada, maka membuat file baru dan menuliskan data
+            echo "create $nama_file_log:";
+            $ok = file_put_contents($nama_file_log, $text_log);
+
+            if ($ok) {
+                echo "OK";
+            } else {
+                echo "FAIL";
+            }
+        } else {
+            echo "ready $nama_file_log:";
+            // Jika file sudah ada, maka menambahkan data ke dalam file
+            $ok = file_put_contents($nama_file_log, $text_log, FILE_APPEND);
+
+            if ($ok) {
+                echo "OK";
+            } else {
+                echo "FAIL";
+            }
+        }
+
+        echo "\n\n";
+
+        $mqtt->close();
     } else {
         // Respons berhasil, Anda dapat mengolah respons sesuai kebutuhan
         $text_log .= "[$timestamp]\nBerhasil merespon \"$topic\" : " . $response . "\n";
