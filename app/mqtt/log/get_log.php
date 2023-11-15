8952a872a8bb5b35810972434ae976af527ccbd3
butoCakil <bennysurahman@gmail.com>
2023-11-16 00:43:47 +0700

    fix + update log

--- app/mqtt/log/get_log.php
+++ app/mqtt/log/get_log.php
@@ -1,14 +1,10 @@
 <?php
-//tanggal dan jam hari ini
 date_default_timezone_set('Asia/Jakarta');
-$timestamp = date('Y-m-d H:i:s');
 $tanggal = date('Y-m-d');
-$jam     = date('H:i:s');
+$getTanggal = @$_GET['date'];
+
+$tanggal = $getTanggal ? $getTanggal : $tanggal;
 
 $logFile = "tag_$tanggal.log";
-
-// Read the content of the log file
 $logContent = file_get_contents($logFile);
-
-// Display the content
 echo nl2br($logContent);
